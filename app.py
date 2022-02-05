96c204645413c6b81bdfb5d3199badf632b5cf2d
Venu <34569918+venuvenk@users.noreply.github.com>
2022-02-05 21:02:11 +0530

    updated  with my home location only

--- app.py
+++ app.py
@@ -10,30 +10,7 @@ st.title('Welcome World!')
 # My Image
 st.image(image, caption='This is me in building the project')
 
-
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-
-data = load_data(10000)
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-
-st.subheader('Map')
+st.subheader('My Home Map')
 my_loc = pd.DataFrame({"lat":[17.50609218738898], "lon": [78.408759753968]})
-st.text(my_loc)
-st.map(my_loc)
 
-st.text(data)
+st.map(my_loc)
